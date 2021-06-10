9362db36ff3ba5a2e19d7979c9c2dde8239a3483
Yuanzhao Zhai <yuanzhaozhai@126.com>
2021-06-10 23:10:20 +0800

    Delete learning_pioneer_controller directory

--- learning_pioneer_controller/test.py
+++
@@ -1,5 +0,0 @@
-import tf
-import numpy as np
-# qtn = tf.transformations.quaternion_from_euler(0, 0, 1, 'rxyz')
-eu = tf.transformations.euler_from_quaternion([0.787673,0.00847994,0.615995,-0.00706603])
-print(eu)
