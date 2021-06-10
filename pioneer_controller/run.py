ec18d83572594f58d8f85e124d05491599cfc7df
Yuanzhao Zhai <yuanzhaozhai@126.com>
2021-06-10 23:22:40 +0800

    Add files via upload

---
+++ pioneer_controller/run.py
@@ -0,0 +1,5 @@
+import os
+
+while(True):
+    os.system("kill -9 $(ps -ef|grep ppo_webots1|gawk '$0 !~/grep/ {print $2}' |tr -s '\n' ' ')")
+    os.system("mpiexec -np 6 timeout 20m python ppo_webots1.py  -nr 6 -np 6")
